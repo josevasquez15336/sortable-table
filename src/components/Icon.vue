2d36d433864f129b546a07f79677dffd88198c70
josevasquez15336 <josevasquez15336@gmail.com>
2022-12-07 12:40:21 -0600

    Feat: add icon component to handle icons as SVG Vue components

---
+++ src/components/Icon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="100%"
+    height="100%"
+    viewBox="0 0 24 24"
+    style="fill: currentColor"
+  >
+    <component :is="iconsMap[props.icon]" />
+  </svg>
+</template>
+<script lang="ts" setup>
+import { PropType } from 'vue'
+import { iconsMap, Icons } from '../utils/iconsMap'
+
+const props = defineProps({
+  icon: {
+    type: String as PropType<Icons>,
+    required: true,
+  },
+})
+</script>
---
+++ src/utils/iconsMap.ts
@@ -0,0 +1,24 @@
+/*
+  SVG Vue components this utility allow extending and async import easily and use SVG  like any other Vue component
+ This method includes all of the goodies you get using Vue. For example, defineAsyncComponent would allow tree shake
+ if the list is too long, get access to things like props and logic if needed.
+ */
+import { defineAsyncComponent } from 'vue'
+
+export const iconsMap = {
+  'sort-up': defineAsyncComponent(
+    () => import('../components/icons/sort-up.vue'),
+  ),
+  'down-arrow': defineAsyncComponent(
+    () => import('../components/icons/down-arrow.vue'),
+  ),
+  'up-arrow': defineAsyncComponent(
+    () => import('../components/icons/up-arrow.vue'),
+  ),
+  'sort-down': defineAsyncComponent(
+    () => import('../components/icons/sort-down.vue'),
+  ),
+  search: defineAsyncComponent(() => import('../components/icons/search.vue')),
+}
+
+export type Icons = keyof typeof iconsMap;
