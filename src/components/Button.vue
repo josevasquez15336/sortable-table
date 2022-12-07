431e0b7693387996613ebabfb1273ad7c1797e1f
josevasquez15336 <josevasquez15336@gmail.com>
2022-12-07 12:43:04 -0600

    Feat: create reusable button component

---
+++ src/components/Button.vue
@@ -0,0 +1,43 @@
+<template>
+  <button
+    type="button"
+    :class="`p-2 mr-2 mb-2 text-sm rounded-lg font-medium focus:outline-none  focus:ring-4 ${buttonClass}`"
+  >
+    {{ label }}
+
+    <Icon
+      v-if="icon !== 'none'"
+      :icon="icon"
+      :class="`${label ? 'ml-2' : 'mx-auto'}`"
+    />
+  </button>
+</template>
+<script lang="ts" setup>
+import { computed, PropType } from 'vue'
+import { Icons } from '../utils/iconsMap'
+import Icon from './Icon.vue'
+
+const buttonStateClassesMap = {
+  default:
+    'text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700',
+  active:
+    'text-white bg-gray-800 hover:bg-gray-900 focus:ring-gray-300  dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700',
+}
+
+const props = defineProps({
+  state: {
+    type: String as PropType<keyof typeof buttonStateClassesMap>,
+    default: 'default',
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+  icon: {
+    type: String as PropType<Icons | 'none'>,
+    default: 'none',
+  },
+})
+
+const buttonClass = computed(() => buttonStateClassesMap[props.state])
+</script>
